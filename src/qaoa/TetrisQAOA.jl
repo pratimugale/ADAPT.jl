61d7ceea6fc045778085228d45b627378439ab2d
Karunya Shirali <karunyashirali@Sophia-MBPro-438349.local>
2025-06-03 12:23:00 -0400

    Don't append Hc before calculating scores
    Earlier version erroneously inserted an Hc layer to the ansatz before the scores are calculated; HOWEVER, calculate_score itself evolves by Hc before evaluating the score

--- src/qaoa/TetrisQAOA.jl
+++ src/qaoa/TetrisQAOA.jl
@@ -222,8 +222,7 @@ function ADAPT.adapt!(
     reference::ADAPT.QuantumState,
     callbacks::ADAPT.CallbackList,
 )
-    # ADD A NEW LAYER
-    insertlayer!(ansatz)
+    p_current = length(ansatz.parameters)
 
     adapted = invoke(ADAPT.adapt!, Tuple{
         ADAPT.AbstractAnsatz,
@@ -234,12 +233,11 @@ function ADAPT.adapt!(
         ADAPT.QuantumState,
         ADAPT.CallbackList,
     }, ansatz, trace, adapt_type, pool, observable, reference, callbacks)
-    if !adapted
-        pop!(ansatz.γ_values)
-        pop!(ansatz.γ_layers)
-        return adapted # STOP IF ADAPTATION IS TERMINATED, REMOVE UNNECESSARY LAYER OF H 
-    end
-    # adapted || return adapted   # STOP IF ADAPTATION IS TERMINATED
+    adapted || return adapted   # STOP IF ADAPTATION IS TERMINATED
+
+    # ADD A NEW LAYER
+    push!(ansatz.γ_values, ansatz.γ0)
+    push!(ansatz.γ_layers, 1+p_current)
 
     return adapted
 end
